ff5e16ec3412d51b9ed147909839e6f8c23e1273
gouzhun <gouzhun@nexa-corp.com>
2018-04-13 10:23:42 +0800

    LoginController

---
+++ Assets/Scenes/Login/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoginController : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () {
+		yield return new WaitForSeconds(3);
+		SceneManager.LoadScene("dashboard");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
